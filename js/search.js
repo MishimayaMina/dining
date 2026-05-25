141040c23958ed82280a2df765c2dd89e3ef3394
Kaniken <73703234+MishimayaMina@users.noreply.github.com>
2026-05-26 03:05:49 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let routes = {};
   const notFoundPage = "notfound.html";
 
-  fetch("https://hidukimura.etranger-d.org/routes/routes.json")
+  fetch("https://dining.etranger-d.org/routes/routes.json")
     .then(res => {
       if (!res.ok) {
         throw new Error("routes.json が読み込めませんでした");
